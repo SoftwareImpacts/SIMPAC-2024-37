e4053001e7bce6621ad64196fd89ed3c0754b987
TabishIzhar <tizhar@iul.ac.in>
2024-01-15 11:04:02 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="PyRCD",
-    version="0.1",
+    version="1.0",
     description="Structural Design and Optimization of Reinforced Concrete Element",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/TabishIzhar/PyRCD.git",
     author="Tabish Izhar",
     author_email="tizhar@iul.ac.in",
-    license="GNU LESSER GENERAL PUBLIC LICENSE v2.1 or later (GNU LGPLv2.1)",
+    license="MIT License",
     classifiers=[
         "Programming Language :: Python :: 3.10.7",
     ],
